03086fb4f121dcda2573fcdb1bdf4a10ba0d7371
shenyunlong.syl <shenyunlong.syl@oceanbase.com>
2022-12-02 20:52:53 +0800

    remove some unnecessary modifications

--- src/main/java/com/alipay/oceanbase/rpc/property/Property.java
+++ src/main/java/com/alipay/oceanbase/rpc/property/Property.java
@@ -28,15 +28,15 @@ public enum Property {
     //remote call related
     RPC_CONNECT_TIMEOUT("rpc.connect.timeout", 1000, "建立RPC连接的超时时间"),
 
-    RPC_CONNECT_TRY_TIMES("rpc.connect.try.times", 1, "建立RPC连接的尝试次数"),
+    RPC_CONNECT_TRY_TIMES("rpc.connect.try.times", 3, "建立RPC连接的尝试次数"),
 
-    RPC_EXECUTE_TIMEOUT("rpc.execute.timeout", 100000, "执行RPC请求的socket超时时间"),
+    RPC_EXECUTE_TIMEOUT("rpc.execute.timeout", 3000, "执行RPC请求的socket超时时间"),
 
     RPC_LOGIN_TIMEOUT("rpc.login.timeout", 1000, "请求RPC登录的超时时间"),
 
-    RPC_LOGIN_TRY_TIMES("rpc.login.try.times", 1, "请求RPC登录的尝试次数"),
+    RPC_LOGIN_TRY_TIMES("rpc.login.try.times", 3, "请求RPC登录的尝试次数"),
 
-    RPC_OPERATION_TIMEOUT("rpc.operation.timeout", 2000000000L, "OB内部执行RPC请求的超时时间"),
+    RPC_OPERATION_TIMEOUT("rpc.operation.timeout", 10000L, "OB内部执行RPC请求的超时时间"),
 
     METADATA_REFRESH_INTERVAL("metadata.refresh.interval", 60000L, "刷新METADATA的时间间隔"),
 
