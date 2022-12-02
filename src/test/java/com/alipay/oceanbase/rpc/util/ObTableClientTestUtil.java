80bca744403089ba58811a77cbd2e5e08c22f455
shenyunlong.syl <shenyunlong.syl@oceanbase.com>
2022-12-02 20:52:53 +0800

    remove some unnecessary modifications

--- src/test/java/com/alipay/oceanbase/rpc/util/ObTableClientTestUtil.java
+++ src/test/java/com/alipay/oceanbase/rpc/util/ObTableClientTestUtil.java
@@ -20,36 +20,20 @@ package com.alipay.oceanbase.rpc.util;
 import com.alipay.oceanbase.rpc.ObTableClient;
 
 public class ObTableClientTestUtil {
-    public static String FULL_USER_NAME          = "root@mysql#ob11.shenyunlong.syl.11.158.77.144";
-    public static String PARAM_URL               = "http://ocp-cfg.alibaba.net:8080/services?User_ID=alibaba&UID=test&Action=ObRootServiceInfo&ObCluster=ob11.shenyunlong.syl.11.158.77.144&database=test";
-    public static String PASSWORD                = "";
-    public static String PROXY_SYS_USER_NAME     = "root";
-    public static String PROXY_SYS_USER_PASSWORD = "";
+    public static String FULL_USER_NAME          = "full-user-name";
+    public static String PARAM_URL               = "config-url";
+    public static String PASSWORD                = "password";
+    public static String PROXY_SYS_USER_NAME     = "sys-user-name";
+    public static String PROXY_SYS_USER_PASSWORD = "sys-user-password";
 
     public static ObTableClient newTestClient() throws Exception {
         ObTableClient obTableClient = new ObTableClient();
-        // for observer directly mode
         obTableClient.setFullUserName(FULL_USER_NAME);
         obTableClient.setParamURL(PARAM_URL);
         obTableClient.setPassword(PASSWORD);
         obTableClient.setSysUserName(PROXY_SYS_USER_NAME);
         obTableClient.setSysPassword(PROXY_SYS_USER_PASSWORD);
 
-        // baigui, for test key and range partition
-//        obTableClient.setOdpAddr("100.88.147.30");
-//        obTableClient.setOdpPort(2885);
-//        obTableClient.setFullUserName("test@mysql#ob96.heshi.zhs");
-//        obTableClient.setPassword("test");
-//        obTableClient.setOdpMode(true);
-//        obTableClient.setDatabase("test");
-
-        // zhiyun hash partition
-//        obTableClient.setOdpAddr("100.88.147.119");
-//        obTableClient.setOdpPort(38887);
-//        obTableClient.setFullUserName("test@mysql#ob96.heshi.zhs");
-//        obTableClient.setPassword("test");
-//        obTableClient.setOdpMode(true);
-//        obTableClient.setDatabase("test");
         return obTableClient;
     }
 }
