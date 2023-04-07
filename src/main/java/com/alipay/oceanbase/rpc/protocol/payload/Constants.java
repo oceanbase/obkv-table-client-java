c3a5b62312e4222a304d2a9b7f1722291f7150d7
chenweixin.cwx <chenweixin.cwx@antgroup.com>
2023-04-07 15:24:31 +0800

    [Fix] constants define error         Link: https://code.alibaba-inc.com/oceanbase/obkv-table-client-java/codereview/12255652
        * fix: fix constants define error
    
    * [Fix] constants define error

--- src/main/java/com/alipay/oceanbase/rpc/protocol/payload/Constants.java
+++ src/main/java/com/alipay/oceanbase/rpc/protocol/payload/Constants.java
@@ -20,14 +20,15 @@ package com.alipay.oceanbase.rpc.protocol.payload;
 public interface Constants {
 
     long  OB_INVALID_ID      = -1;
+    
     long  INVALID_TABLET_ID  = 0;
 
-    short UNSIGNED_INT8_MAX  = 1 << 8 - 1;
+    short UNSIGNED_INT8_MAX  = (1 << 8) - 1;
 
-    int   UNSIGNED_INT16_MAX = 1 << 16 - 1;
+    int   UNSIGNED_INT16_MAX = (1 << 16) - 1;
 
-    int   UNSIGNED_INT24_MAX = 1 << 24 - 1;
+    int   UNSIGNED_INT24_MAX = (1 << 24) - 1;
 
-    long  UNSIGNED_INT32_MAX = 1L << 32 - 1;
+    long  UNSIGNED_INT32_MAX = (1L << 32) - 1;
 
 }
