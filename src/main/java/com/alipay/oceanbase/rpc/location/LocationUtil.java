748a52b8681a39cfef31f895a92fe87e06828317
maochongxin <maochongxin@gmail.com>
2024-09-19 19:20:42 +0800

    Optimize SQL for refreshing table location information

--- src/main/java/com/alipay/oceanbase/rpc/location/LocationUtil.java
+++ src/main/java/com/alipay/oceanbase/rpc/location/LocationUtil.java
@@ -153,13 +153,19 @@ public class LocationUtil {
                                                                               + "FROM oceanbase.__all_virtual_proxy_schema A inner join oceanbase.__all_server B on A.svr_ip = B.svr_ip and A.sql_port = B.inner_port "
                                                                               + "WHERE tenant_name = ? and database_name=? and table_name = ? and tablet_id = 0";
 
-    private static final String PROXY_LOCATION_SQL_PARTITION_V4  = "SELECT /*+READ_CONSISTENCY(WEAK)*/ A.tablet_id as tablet_id, A.svr_ip as svr_ip, A.sql_port as sql_port, "
-                                                                   + "A.table_id as table_id, A.role as role, A.replica_num as replica_num, A.part_num as part_num, B.svr_port as svr_port, B.status as status, B.stop_time as stop_time "
-                                                                   + ", A.spare1 as replica_type, D.ls_id as ls_id "
-                                                                   + "FROM oceanbase.__all_virtual_proxy_schema A inner join oceanbase.__all_server B on A.svr_ip = B.svr_ip and A.sql_port = B.inner_port "
-                                                                   + "inner join oceanbase.DBA_OB_TENANTS C on C.tenant_name = A.tenant_name "
-                                                                   + "left join oceanbase.CDB_OB_TABLET_TO_LS D on D.tenant_id = C.tenant_id and D.tablet_id = A.tablet_id "
-                                                                   + "WHERE C.tenant_name = ? and database_name= ? and table_name = ? and A.tablet_id in ({0}) ";
+    private static final String PROXY_LOCATION_SQL_PARTITION_V4            = "SELECT /*+READ_CONSISTENCY(WEAK)*/ * FROM ( "
+                                                                            + "   SELECT A.tablet_id as tablet__id, A.svr_ip as svr_ip, A.sql_port as sql_port, A.table_id as table_id, "
+                                                                            + "   A.role as role, A.replica_num as replica_num, A.part_num as part_num, B.svr_port as svr_port, B.status as status, "
+                                                                            + "   B.stop_time as stop_time, A.spare1 as replica_type "
+                                                                            + "   FROM oceanbase.__all_virtual_proxy_schema A "
+                                                                            + "   INNER JOIN oceanbase.__all_server B ON A.svr_ip = B.svr_ip AND A.sql_port = B.inner_port "
+                                                                            + "   WHERE A.tablet_id IN ({0}) AND A.tenant_name = ? AND A.database_name = ? AND A.table_name = ?) AS left_table "
+                                                                            + "LEFT JOIN ("
+                                                                            + "   SELECT D.ls_id, D.tablet_id "
+                                                                            + "   FROM oceanbase.__all_virtual_tablet_to_ls D "
+                                                                            + "   INNER JOIN oceanbase.DBA_OB_TENANTS C ON D.tenant_id = C.tenant_id "
+                                                                            + "   WHERE C.tenant_name = ? "
+                                                                            + ") AS right_table ON left_table.tablet__id = right_table.tablet_id;";
 
     private static final String PROXY_FIRST_PARTITION_SQL_V4                = "SELECT /*+READ_CONSISTENCY(WEAK)*/ part_id, part_name, tablet_id, high_bound_val, sub_part_num "
                                                                               + "FROM oceanbase.__all_virtual_proxy_partition "
@@ -798,6 +804,7 @@ public class LocationUtil {
                 ps.setString(1, key.getTenantName());
                 ps.setString(2, key.getDatabaseName());
                 ps.setString(3, key.getTableName());
+                ps.setString(4, key.getTenantName());
                 rs = ps.executeQuery();
                 partitionEntry = getPartitionLocationFromResultSet(tableEntry, rs, partitionEntry);
             } catch (Exception e) {
