40b995c5c43c60a23fc2dd0ca25653b5360f3956
maochongxin <maochongxin@gmail.com>
2024-10-23 20:09:40 +0800

    add -4138

--- src/main/java/com/alipay/oceanbase/rpc/bolt/transport/ObTableRemoting.java
+++ src/main/java/com/alipay/oceanbase/rpc/bolt/transport/ObTableRemoting.java
@@ -31,8 +31,6 @@ import com.alipay.remoting.exception.RemotingException;
 import io.netty.buffer.ByteBuf;
 import org.slf4j.Logger;
 
-import javax.xml.transform.Result;
-
 import static com.alipay.oceanbase.rpc.protocol.packet.ObCompressType.INVALID_COMPRESSOR;
 import static com.alipay.oceanbase.rpc.protocol.packet.ObCompressType.NONE_COMPRESSOR;
 
