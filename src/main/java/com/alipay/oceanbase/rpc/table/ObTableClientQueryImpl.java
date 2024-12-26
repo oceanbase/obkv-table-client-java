9076737a64bba54521af4943f57541b2e80f79e4
JackShi148 <jackshi896@gmail.com>
2024-12-26 10:35:07 +0800

    revert odp operations to old client version, do not send tablet_id to odp

--- src/main/java/com/alipay/oceanbase/rpc/table/ObTableClientQueryImpl.java
+++ src/main/java/com/alipay/oceanbase/rpc/table/ObTableClientQueryImpl.java
@@ -18,6 +18,7 @@
 package com.alipay.oceanbase.rpc.table;
 
 import com.alipay.oceanbase.rpc.ObTableClient;
+import com.alipay.oceanbase.rpc.exception.FeatureNotSupportedException;
 import com.alipay.oceanbase.rpc.exception.ObTableException;
 import com.alipay.oceanbase.rpc.location.model.partition.ObPair;
 import com.alipay.oceanbase.rpc.mutation.Row;
@@ -169,15 +170,24 @@ public class ObTableClientQueryImpl extends AbstractTableQueryImpl {
                     throw new ObTableException("key range columns must be specified when use index");
                 }
             }
-            if (tableQuery.getIndexName() != null) {
-                this.partitionObTables.put(0L, new ObPair<Long, ObTableParam>(0L, new ObTableParam(
+            if (getPartId() != null && tableQuery.getIndexName() == null) {
+                try {
+                    ObPair<Long, ObTableParam> odpTable = obTableClient.getODPTableWithPartId(
+                            tableName, getPartId(), false);
+                    partitionObTables.put(odpTable.getLeft(), odpTable);
+                } catch (Exception e) {
+                    if (e instanceof ObTableException) {
+                        if (((ObTableException) e).getErrorCode() == ResultCodes.OB_NOT_SUPPORTED.errorCode) {
+                            // current ODP version does not support get partition meta information
+                            throw new FeatureNotSupportedException("current ODP version does not support query with part id", e);
+                        }
+                    } else {
+                        throw e;
+                    }
+                }
+            }  else {
+                partitionObTables.put(0L, new ObPair<Long, ObTableParam>(0L, new ObTableParam(
                         obTableClient.getOdpTable())));
-            } else if (getPartId() == null) {
-                initPartitions();
-            } else {
-                ObPair<Long, ObTableParam> odpTable = obTableClient.getODPTableWithPartId(
-                    tableName, getPartId(), false);
-                this.partitionObTables.put(odpTable.getLeft(), odpTable);
             }
         } else {
             if (getPartId() == null) {
@@ -280,14 +290,8 @@ public class ObTableClientQueryImpl extends AbstractTableQueryImpl {
             }
             ObBorderFlag borderFlag = range.getBorderFlag();
             // pairs -> List<Pair<logicId, param>>
-            List<ObPair<Long, ObTableParam>> pairs = null;
-            if (!this.obTableClient.isOdpMode()) {
-                pairs = this.obTableClient.getTables(indexTableName, tableQuery, start,
-                    borderFlag.isInclusiveStart(), end, borderFlag.isInclusiveEnd(), false, false);
-            } else {
-                pairs = this.obTableClient.getOdpTables(tableName, tableQuery, start,
-                    borderFlag.isInclusiveStart(), end, borderFlag.isInclusiveEnd(), false);
-            }
+            List<ObPair<Long, ObTableParam>> pairs = this.obTableClient.getTables(indexTableName, tableQuery, start,
+                borderFlag.isInclusiveStart(), end, borderFlag.isInclusiveEnd(), false, false);
             if (tableQuery.getScanOrder() == ObScanOrder.Reverse) {
                 for (int i = pairs.size() - 1; i >= 0; i--) {
                     partitionObTables.put(pairs.get(i).getLeft(), pairs.get(i));
