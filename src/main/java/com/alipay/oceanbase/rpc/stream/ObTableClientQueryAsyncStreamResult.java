32da54e8e8dd0b45bf5e187c32ea8b905e1b6568
maochongxin <maochongxin@gmail.com>
2025-01-07 10:36:16 +0800

    set query table name to lower case

--- src/main/java/com/alipay/oceanbase/rpc/stream/ObTableClientQueryAsyncStreamResult.java
+++ src/main/java/com/alipay/oceanbase/rpc/stream/ObTableClientQueryAsyncStreamResult.java
@@ -56,7 +56,7 @@ public class ObTableClientQueryAsyncStreamResult extends AbstractQueryStreamResu
         int maxRetries = client.getTableEntryRefreshTryTimes();
         // init request
         ObTableQueryRequest request = new ObTableQueryRequest();
-        request.setTableName(tableName);
+        request.setTableName(tableName.toLowerCase(Locale.ROOT));
         request.setTableQuery(tableQuery);
         request.setEntityType(entityType);
         request.setConsistencyLevel(getReadConsistency().toObTableConsistencyLevel());
