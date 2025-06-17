8012898da8933e3256ac097bc8bd697bed5775b1
JackShi148 <jackshi896@gmail.com>
2025-06-17 11:33:36 +0800

    mvn complie passed

--- src/main/java/com/alipay/oceanbase/rpc/bolt/transport/ObConfigurableInstance.java
+++ src/main/java/com/alipay/oceanbase/rpc/bolt/transport/ObConfigurableInstance.java
@@ -17,14 +17,69 @@
 
 package com.alipay.oceanbase.rpc.bolt.transport;
 
-import com.alipay.remoting.config.AbstractConfigurableInstance;
+import com.alipay.remoting.*;
+import com.alipay.remoting.config.ConfigManager;
+import com.alipay.remoting.config.ConfigurableInstance;
+import com.alipay.remoting.config.configs.ConfigContainer;
+import com.alipay.remoting.config.configs.ConfigItem;
 import com.alipay.remoting.config.configs.ConfigType;
+import com.alipay.remoting.config.configs.DefaultConfigContainer;
+import com.alipay.remoting.config.switches.GlobalSwitch;
+import com.alipay.remoting.exception.RemotingException;
+import com.alipay.remoting.rpc.RpcResponseFuture;
+import com.alipay.remoting.rpc.protocol.UserProcessor;
 
-public class ObConfigurableInstance extends AbstractConfigurableInstance {
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ObConfigurableInstance implements ConfigurableInstance {
+    private ConfigContainer configContainer = new DefaultConfigContainer();
+    private GlobalSwitch    globalSwitch    = new GlobalSwitch();
+    private ConfigType      configType;
+
     /*
      * Ob configurable instance.
      */
     public ObConfigurableInstance() {
-        super(ConfigType.CLIENT_SIDE);
+        this.configType = ConfigType.CLIENT_SIDE;
+    }
+
+    @Override
+    public ConfigContainer conf() {
+        return this.configContainer;
+    }
+
+    @Override
+    public GlobalSwitch switches() {
+        return this.globalSwitch;
+    }
+
+    @Override
+    public void initWriteBufferWaterMark(int low, int high) {
+        this.configContainer.set(configType, ConfigItem.NETTY_BUFFER_LOW_WATER_MARK, low);
+        this.configContainer.set(configType, ConfigItem.NETTY_BUFFER_HIGH_WATER_MARK, high);
+    }
+
+    @Override
+    public int netty_buffer_low_watermark() {
+        if (null != configContainer
+            && configContainer.contains(configType, ConfigItem.NETTY_BUFFER_LOW_WATER_MARK)) {
+            return (Integer) configContainer
+                .get(configType, ConfigItem.NETTY_BUFFER_LOW_WATER_MARK);
+        } else {
+            return ConfigManager.netty_buffer_low_watermark();
+        }
+    }
+
+    @Override
+    public int netty_buffer_high_watermark() {
+        if (null != configContainer
+            && configContainer.contains(configType, ConfigItem.NETTY_BUFFER_HIGH_WATER_MARK)) {
+            return (Integer) configContainer.get(configType,
+                ConfigItem.NETTY_BUFFER_HIGH_WATER_MARK);
+        } else {
+            return ConfigManager.netty_buffer_high_watermark();
+        }
     }
 }
