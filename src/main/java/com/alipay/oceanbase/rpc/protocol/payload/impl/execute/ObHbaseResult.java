a644abab9456b01e10a48ca8237bdb0ce7059b52
vanson <43193589+WeiXinChan@users.noreply.github.com>
2025-07-21 12:33:56 +0800

    new hbase protocal (#363)

---
+++ src/main/java/com/alipay/oceanbase/rpc/protocol/payload/impl/execute/ObHbaseResult.java
@@ -0,0 +1,48 @@
+/*-
+ * #%L
+ * com.oceanbase:obkv-table-client
+ * %%
+ * Copyright (C) 2021 - 2024 OceanBase
+ * %%
+ * OBKV Table Client Framework is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * #L%
+ */
+
+package com.alipay.oceanbase.rpc.protocol.payload.impl.execute;
+
+import com.alipay.oceanbase.rpc.protocol.payload.AbstractPayload;
+import com.alipay.oceanbase.rpc.protocol.payload.Pcodes;
+
+public class ObHbaseResult extends AbstractPayload {
+
+    /*
+     * Get pcode.
+     */
+    @Override
+    public int getPcode() {
+        return Pcodes.OB_TABLE_API_HBASE_EXECUTE;
+    }
+
+    @Override
+    public byte[] encode() {
+        byte[] bytes = new byte[(int) getPayloadSize()];
+        int idx = 0;
+
+        // 0. encode header
+        idx = encodeHeader(bytes, idx);
+
+        return bytes;
+    }
+
+    @Override
+    public long getPayloadContentSize() {
+        return 0;
+    }
+}
